20455088d8c487f9db426c88fdca929e22c0fb5e
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2026-01-05 05:56:19 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home').then(m => m.Home)} ,
+    { path:'todos',
+      loadComponent: () => import('./todos/todos').then(m => m.Todos),  
+    }
+      
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
+
 import { Header } from './components/header/header';
 
 
 @Component({
   selector: 'app-root',
-imports: [RouterOutlet,Home,Header
+imports: [RouterOutlet,Header
   ],
   template: `
   <app-header/>
   <main> 
-  <app-home/>
+  <router-outlet/>
   </main> 
   `,
   styles: [`
